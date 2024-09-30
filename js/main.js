2bfad87fe9ce467807f9ac9705404c211bbdfbf1
MikhaelKreslavskii <116979503+MikhaelKreslavskii@users.noreply.github.com>
2024-10-01 00:00:52 +0300

    Add js

---
+++ js/main.js
@@ -0,0 +1,11 @@
+const nav = document.querySelector("#nav");
+const navBtn = document.querySelector("#nav-btn");
+const navBtnImg = document.querySelector("#nav-btn-img");
+
+navBtn.onclick = () => {
+  if (nav.classList.toggle("open")) {
+    navBtnImg.src = "./img/Icons/nav_close.svg";
+  } else {
+    navBtnImg.src = "./img/Icons/nav_open.svg";
+  }
+};
